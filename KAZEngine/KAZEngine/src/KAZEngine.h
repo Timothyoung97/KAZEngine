07b25f1862941f6295b4bca68def60c1817647ae
Timothy Yang <timothyoung97@gmail.com>
2022-03-23 03:47:12 +0800

    Set up logging system
    Reference: https://www.youtube.com/watch?v=dZr-53LAlOw&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=6&ab_channel=TheCherno
    
    3rd Party Library Used: https://github.com/gabime/spdlog
    
    1. Learn on how to utilize 3rd party asset (import & setting up of project directories)
    2. Learn on how to include header files

--- KAZEngine/KAZEngine/src/KAZEngine.h
+++ KAZEngine/KAZEngine/src/KAZEngine.h
@@ -3,6 +3,7 @@
 // For use by KAZEngine Applications
 #include <stdio.h>
 #include "KAZEngine/Application.h"
+#include "KAZEngine/Log.h"
 
 // -----EntryPoint-----
 #include "KAZEngine/EntryPoint.h"
---
+++ KAZEngine/KAZEngine/src/KAZEngine/Log.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+
+namespace KAZEngine 
+{
+
+	class KAZENGINE_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+		
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+
+}
+
+// Core log marcos
+#define KAZENGINE_CORE_TRACE(...)	::KAZEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define KAZENGINE_CORE_INFO(...)	::KAZEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define KAZENGINE_CORE_WARN(...)	::KAZEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define KAZENGINE_CORE_ERROR(...)	::KAZEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define KAZENGINE_CORE_FATAL(...)	::KAZEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log marcos
+#define KAZENGINE_TRACE(...)		::KAZEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define KAZENGINE_INFO(...)			::KAZEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define KAZENGINE_WARN(...)			::KAZEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define KAZENGINE_ERROR(...)		::KAZEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define KAZENGINE_FATAL(...)		::KAZEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
