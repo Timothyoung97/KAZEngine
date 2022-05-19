89999d176f17cf679620a3677dbcf18404b70f22
Timothy Yang <timothyoung97@gmail.com>
2022-05-20 00:15:23 +0800

    Window Events
    Reference: https://www.youtube.com/watch?v=r74WxFMIEdU&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=12&ab_channel=TheCherno
    
    1. Presence of Dispatcher Software Dev Pattern
    2. Linking up event response to input

--- KAZEngine/KAZEngine/src/KAZEngine/Application.cpp
+++ KAZEngine/KAZEngine/src/KAZEngine/Application.cpp
@@ -1,20 +1,31 @@
 #include "kepch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
 #include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace KAZEngine
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		KAZENGINE_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -25,5 +36,11 @@ namespace KAZEngine
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
